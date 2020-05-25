ce69a5f34faae606a2150346ee4ad710dcc0236c
Porkchopsss <pokoshih@gmail.com>
2020-05-25 14:09:30 +1000

    initial upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var myInput = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
