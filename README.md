8d92bbd7d6984099d7bf110519681c93cab9b748
Porkchopsss <pokoshih@gmail.com>
2020-05-30 09:57:54 +1000

    Uploading my working generator

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Password-Generator
+# 03 JavaScript: Password Generator
+
+This application generates a random password between 8-128 with alphabets,uppercase, numbers, and/or symbols.
+
+The app will prompt your input before you generate your password in the textarea.
+
+My Password Generator shown in the following:
+<img src="images/Password Generator.PNG">
+
+## Review
+
+You are required to submit the following for review:
+
+* The URL of the deployed application.
+
+* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+
+
--- script.js
+++ script.js
@@ -1,18 +1,51 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-var myInput = document.getElementById("psw");
-var letter = document.getElementById("letter");
-var capital = document.getElementById("capital");
-var number = document.getElementById("number");
-var length = document.getElementById("length");
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Assignment Javascript
+// start with setting the variables
 
-  passwordText.value = password;
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*()?";
+var generateBtn=document.querySelector("#generateBtn");
 
-}
+// Do While loop, the code will pass on only if while statement is "false" as "true" is incorrect and will keep prompting the user
+do {
+  var length = prompt("How long would you like your password? Try to stick to 8-128 long);   
+  }
+  while (8 > length || length > 128);
+  
+  var LowerOrnot = confirm("Would you like alphabets?");
+  var UpperOrNot = confirm("Would you like Uppercase?");
+  var NumbersOrNot = confirm("Would you like Numbers?");
+  var SymbolsOrNot = confirm("Would you like Symbols?");
+    // addEventListener for click.
+  generateBtn.addEventListener("click",function (){
+      var password= generatePassword();
+      console.log(password);
+      var textarea = document.getElementById("password");
+      textarea.innerHTML = password;
+  });
+  // generatePassword function using if statement
+  function generatePassword(){
+      var allowed = '';
+      if(LowerOrnot){
+          allowed+= lowercase;
+      }
+      if(UpperOrNot){
+          allowed += uppercase;
+      }
+      if(NumbersOrNot){
+          allowed += numbers;
+      }
+      if(SymbolsOrNot){
+          allowed += symbols;
+      }    
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+      var password = '';
+      for(var i = 0; i<length; i++){
+          var pwindex= Math.floor(Math.random()*allowed.length);
+          password += allowed[pwindex];
+      }
+      return password;
+      
+      
+  }
